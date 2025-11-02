394336fe23fd35bab0b0baa58b19d79ecfa922d7
Miroslav Kravec <kravec.miroslav@gmail.com>
2025-11-02 10:14:40 +0100

    feat: render Mermaid to SVG at build time

---
+++ bin/build-mermaid.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# adjusted from source: https://shendriks.dev/posts/2024-04-19-using-hugo-with-mermaid-wo-javascript/
+
+set -euo pipefail
+
+while IFS= read -r -d '' file
+do
+  DIR=$(dirname "$file")
+  mmdc $MMDC_EXTRA_OPTIONS -i "${DIR}"/index.md -o "${DIR}"/mermaid-out.svg
+
+  # Fix numbering of output files considering all code blocks found in the .md file
+  readarray -t number_map < <(grep -E '^```([a-zA-Z0-9]+)' "${DIR}"/index.md | nl -w1 | grep mermaid | awk '{print $1}' | nl -w1 -s:)
+  for value in "${number_map[@]}"
+  do
+    readarray -t numbers < <(echo "$value" | tr ":" "\n")
+    if [[ "${numbers[0]}" != "${numbers[1]}" ]]; then
+      mv -v "${DIR}"/mermaid-out-"${numbers[0]}".svg "${DIR}"/mermaid-out-"${numbers[1]}".svg
+    fi
+  done
+done<   <(find content/ -name index.md -print0)
--- shell.nix
+++ shell.nix
@@ -7,10 +7,18 @@ let
         url = "https://github.com/NixOS/nixpkgs/archive/4684fd6b0c01e4b7d99027a34c93c2e09ecafee2.tar.gz";
     }) {};
 
+    mermaidLockedPkgs = import (builtins.fetchTarball {
+        # for mermaid '11.4.2'
+        # search with https://lazamar.co.uk/nix-versions/
+        url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
+    }) {};
+
     hugo = hugoLockedPkgs.hugo;
+    mermaid-cli = mermaidLockedPkgs.mermaid-cli;
 
 in pkgs.mkShellNoCC {
   packages = [
     hugo
+    mermaid-cli
   ];
 }
